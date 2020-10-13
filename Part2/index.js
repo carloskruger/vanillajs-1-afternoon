74ead5fef5f3e7eda1bf36a268e1efb471ccf1d1
Carlos Kruger <carloskr@yahoo.com>
2020-10-13 19:55:34 -0400

    part2

---
+++ Part2/index.js
@@ -0,0 +1,17 @@
+console.log("The house always wins!")
+
+let idInput = document.getElementById('idInput');   
+let colorInput = document.getElementById('colorInput')
+
+    function setCard(){
+        
+        const card = document.getElementById(idInput.value);
+        card.style.color = colorInput.value;
+    
+    
+    }
+
+
+
+
+
